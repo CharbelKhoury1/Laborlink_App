d7ec54e7675c1b03488e98d5b5bf008b586bb8e5
Charbel Khoury <118617062+CharbelKhoury1@users.noreply.github.com>
2025-06-28 19:12:14 +0300

    Updated ErrorBoundary.tsx

--- components/InlineError.tsx
+++ components/InlineError.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { AlertCircle, X } from 'lucide-react-native';
+import { CircleAlert as AlertCircle, X } from 'lucide-react-native';
 import Colors from '@/constants/Colors';
 
 interface InlineErrorProps {

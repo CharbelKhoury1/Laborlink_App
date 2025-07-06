c5cc88ef2526f223e43e72f4c5a2a1779e8f110a
Charbelkhoury123 <Charbelkhoury1121@gmail.com>
2025-07-06 19:56:10 +0300

    Updated _layout.tsx

--- app/(tabs)/profile/_layout.tsx
+++ app/(tabs)/profile/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function ProfileLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="home" />
+      <Stack.Screen name="index" />
       <Stack.Screen name="edit" />
       <Stack.Screen name="privacy" />
       <Stack.Screen name="settings" />
